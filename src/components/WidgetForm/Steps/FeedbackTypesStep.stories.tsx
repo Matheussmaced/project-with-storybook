15a067887dec141bdfbadb25488b9f4f08538377
Matheussmaced <mateusbyakuya3@gmail.com>
2025-02-16 12:21:57 -0300

    feat: creating feedback storie, with external information

--- src/components/WidgetForm/Steps/FeedbackTypesStep.stories.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypesStep.stories.tsx
@@ -1,10 +1,29 @@
 
 import { Meta, StoryObj } from '@storybook/react';
-import { FeedbackContentStep } from './FeedbackContentStep';
+import { Popover } from '@headlessui/react';
+import { FeedbackTypeStep } from './FeedbackTypeStep';
 
 export default {
   title: 'Widget UI/FeedbackTypeStep',
-  component: FeedbackContentStep,
+  component: FeedbackTypeStep,
+  decorators: [
+    (Story) => {
+      return (
+        <Popover>
+          <Popover.Panel static>
+            <div className='bg-zinc-900 relative p-4 rounded-lg flex flex-col items-center w-96'>
+              {Story()}
+            </div>
+          </Popover.Panel>
+        </Popover>
+      )
+    }
+  ] // contexto para o componente funcionar (Se ele depender de informações externas para funcionar)
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj = {
+  args: {
+    image: { src: 'https://via.placeholder.com/150', alt: 'Imagem de teste' },
+    text: 'Exemplo de feedback',
+  },
+}
